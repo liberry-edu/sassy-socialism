18bde1b8c8ee95306d329e240b0f1ef873781854
Pushkar Sharma <thePsguy@icloud.com>
2016-06-23 01:29:57 +0530

    api calls back to /api and removed useless comments

--- app/scripts/learn/category.controller.js
+++ app/scripts/learn/category.controller.js
@@ -14,45 +14,6 @@ define(["./learn.module"], function () {
    */
   angular.module('liberry.learnModule')
     .controller('learn.CategoryCtrl', ["$scope", "$stateParams","$http", function ($scope, $stateParams, $http) {
-      // $scope.contents = [
-      //   {
-      //     "key": "1232145",
-      //     "name": "Video: Meet Rahul ",
-      //     "desc": "Interesting Video",
-      //     "contenturl": "content/demo.mp4",
-      //     "type": "video"
-      //   },
-      //   {
-      //     "key": "12321245",
-      //     "name": "Rahul meets Tina",
-      //     "contenturl": "images/bl1.jpg",
-      //     "type": "doc"
-      //   },
-      //   {
-      //     "key": "1732145",
-      //     "name": "Tina meets Kajal",
-      //     "contenturl": "images/bl1.jpg",
-      //     "type": "audio"
-      //   },
-      //   {
-      //     "key": "632145",
-      //     "name": "Kajal meets Nimit",
-      //     "contenturl": "images/bl1.jpg",
-      //     "type": "game"
-      //   },
-      //   {
-      //     "key": "92145",
-      //     "name": "Nimit meets Ahilya",
-      //     "contenturl": "content/demo.mp4",
-      //     "type": "video"
-      //   },
-      //   {
-      //     "key": "6145",
-      //     "name": "Ahilya meets Suhas",
-      //     "contenturl": "content/demo.mp4",
-      //     "type": "video"
-      //   }
-      // ];
 
       $scope.content =
   {
@@ -71,7 +32,7 @@ define(["./learn.module"], function () {
   }
 
 
-      $http({method: 'GET', url: 'http://52.77.111.177:8080/api/modules?filters={"parent_id":null, "category_id": ' + $stateParams.categoryId + '}', headers: {
+      $http({method: 'GET', url: '/api/modules?filters={"parent_id":null, "category_id": ' + $stateParams.categoryId + '}', headers: {
           'Authorization': 'Basic cHM6cGFzc3dvcmQ='
         }
       }).then(function(res){
@@ -80,13 +41,6 @@ define(["./learn.module"], function () {
       }, function(er){
         console.log(er);
       });
-      // for (var i = 0; i < $scope.contents.length; i++) {
-      //   if ($stateParams.contentId === $scope.contents[i].key) {
-      //     $scope.content = $scope.contents[i];
-      //   }
-      // }
-      //console.log($scope.content);
-      console.log($stateParams);
-
+      
     }]);
 });
