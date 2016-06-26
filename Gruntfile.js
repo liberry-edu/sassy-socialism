7eb1bbcd5ce04a0f45e9078a43e77ebee46711ce
Abhishek Aggarwal <abhishek.fire7@gmail.com>
2016-06-26 19:38:55 +0530

    port set to 20000

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,10 +84,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 20000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 21000
       },
       livereload: {
         options: {
